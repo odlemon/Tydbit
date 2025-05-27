9de8c9e624eac41c8d1ef3a18ecc9a1090846fbd
Nyasha Karata <veximagames@gmail.com>
2025-05-27 08:21:05 +0200

    Typo analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod cli;
+mod analyzer;
+
+use analyzer::typos;
+use std::path::Path;
 
 fn main() {
     let args = cli::get_args();
+    let dir = Path::new(&args.path);
 
-    println!("🔍 Scanning directory: {}", args.path.display());
+    typos::scan_for_typos(dir);
 }
